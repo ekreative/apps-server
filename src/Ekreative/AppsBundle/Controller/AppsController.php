d5e5ec31a8e89559875ded7aee8964fbe75a9d54
av_tehnik <vitaliy@ekreative.com>
2014-08-01 16:50:32 +0300

    add support filenames

--- src/Ekreative/AppsBundle/Controller/AppsController.php
+++ src/Ekreative/AppsBundle/Controller/AppsController.php
@@ -30,6 +30,10 @@ class AppsController extends Controller {
             $app = new \Ekreative\AppsBundle\Entity\App();
             $app->setFolder($folder);
             $form = $this->newAppForm($app);
+
+
+
+
             return $this->render('EkreativeAppsBundle:Apps:index.html.twig', array(
                         'folder' => $folder,
                         'appform' => $form->createView(),
@@ -49,8 +53,8 @@ class AppsController extends Controller {
         $app = new \Ekreative\AppsBundle\Entity\App();
         $app->setFolder($folder);
         $app->setDate(new \DateTime());
-        
-        
+
+
         $form = $this->newAppForm($app);
 
         $form->handleRequest($request);
@@ -60,7 +64,12 @@ class AppsController extends Controller {
         $app->updateFile();
         $em->flush();
 
-        $url = 'http://' . $_SERVER['HTTP_HOST'] . '/apps/' . $folderId . '/' . $app->getId() . '.apk';
+
+
+
+        $url = $this->generateUrl('ekreative_download_app', array('folder' => $folderId, 'id' => $app->getId()), true);
+
+//        $url = 'http://' . $_SERVER['HTTP_HOST'] . '/apps/' . $folderId . '/' . $app->getId() . '.apk';
         $qrcode = 'http://chart.apis.google.com/chart?chl=' . urlencode($url) . '&chs=200x200&choe=UTF-8&cht=qr&chld=L%7C2';
         $app->setQrcode($qrcode);
 
@@ -81,18 +90,37 @@ class AppsController extends Controller {
             $em = $this->getDoctrine()->getManager();
             $em->remove($app);
             $em->flush();
-            unlink($this->appsFolder.DIRECTORY_SEPARATOR.$filderId.DIRECTORY_SEPARATOR.$id.'.apk');
+            unlink($this->appsFolder . DIRECTORY_SEPARATOR . $filderId . DIRECTORY_SEPARATOR . $id . '.apk');
             return new \Symfony\Component\HttpFoundation\RedirectResponse($this->generateUrl('ekreative_folder', array('id' => $filderId)));
         }
         return new \Symfony\Component\HttpFoundation\RedirectResponse($this->generateUrl('ekreative_folder', array('id' => $filderId)));
     }
 
+    public function downloadAction(Request $request, $folder, $id) {
+
+        $app = $this->getDoctrine()->getRepository('EkreativeAppsBundle:App')->find($id);
+        
+        if ($app) {
+            $response = new \Symfony\Component\HttpFoundation\Response();
+            $filename = $this->appsFolder . DIRECTORY_SEPARATOR . $folder . DIRECTORY_SEPARATOR . $id . '.apk';
+            // Set headers
+            $response->headers->set('Cache-Control', 'private');
+            $response->headers->set('Content-type', mime_content_type($filename));
+            $response->headers->set('Content-Disposition', 'attachment; filename="' . $app->getFile() . '";');
+            $response->headers->set('Content-length', filesize($filename));
+            // Send headers before outputting anything
+            $response->sendHeaders();
+            return $response->setContent(readfile($filename));
+        }
+        return new \Symfony\Component\HttpFoundation\RedirectResponse($this->generateUrl('ekreative_folder', array('id' => $folder)));
+    }
+
     private function newAppForm($entity) {
         return $this->createFormBuilder($entity)
-                        ->add('uploadedFile', 'file', array('attr'=>array('placeholder'=>'version','class'=>"form-control")))
-                        ->add('version', 'text', array('required'=>false, 'attr'=>array('placeholder'=>'version','class'=>"form-control")))
-                        ->add('comment', 'text', array('required'=>false, 'attr'=>array('placeholder'=>'comment','class'=>"form-control")))
-                        ->setAction($this->generateUrl('ekreative_new_app',array('id'=>$entity->getFolder()->getId())))
+                        ->add('uploadedFile', 'file', array('attr' => array('placeholder' => 'version', 'class' => "form-control")))
+                        ->add('version', 'text', array('required' => false, 'attr' => array('placeholder' => 'version', 'class' => "form-control")))
+                        ->add('comment', 'text', array('required' => false, 'attr' => array('placeholder' => 'comment', 'class' => "form-control")))
+                        ->setAction($this->generateUrl('ekreative_new_app', array('id' => $entity->getFolder()->getId())))
                         ->setMethod('POST')
                         ->add('save', 'submit')
                         ->getForm();
