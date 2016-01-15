6b1ecb3f630626343470a7c1c20721ec15565628
Fred Cox <fred@ekreative.com>
2016-01-15 17:19:26 +0200

    remove http cache

--- web/app.php
+++ web/app.php
@@ -9,18 +9,17 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // You should change the ApcClassLoader first argument to a unique prefix
 // in order to prevent cache key conflicts with other applications
 // also using APC.
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+
+$apcLoader = new ApcClassLoader('as', $loader);
 $loader->unregister();
 $apcLoader->register(true);
-*/
 
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
