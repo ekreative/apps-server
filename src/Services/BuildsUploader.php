0a7541601302f4ac00dc87799945b7f02aa4778e
Fred Cox <mcfedr@gmail.com>
2019-03-19 12:00:18 +0200

    in uploader use new qrcode

--- src/Services/BuildsUploader.php
+++ src/Services/BuildsUploader.php
@@ -60,9 +60,7 @@ class BuildsUploader
     public function upload(UploadedFile $file, $comment, $project, $type, $ref = null, $commit = null, $jobName = null, $ci = false)
     {
         $app = $this->appFactory->create($file, $comment, $project, $type, $ref, $commit, $jobName, $ci);
-
-        $app->setQrcodeUrl('https://chart.apis.google.com/chart?chl=' . urlencode($this->router->generate('build_install_platform',
-                ['token' => $app->getToken(), 'platform' => $app->getType()], UrlGeneratorInterface::ABSOLUTE_URL)) . '&chs=200x200&choe=UTF-8&cht=qr&chld=L%7C2');
+        $app->setQrcodeUrl($this->router->generate('build_install_qr', ['token' => $app->getToken()]));
 
         if ($app->getBuildUrl()) {
             $this->appDataManager->saveJsonData($app);
